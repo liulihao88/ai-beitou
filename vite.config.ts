0bde0f1e6d979112cf1d74895bc13e38806e4fb2
liulihao <551772063@qq.com>
2025-02-28 13:57:33 +0800

    feat: 基础配置

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import { codeInspectorPlugin } from 'code-inspector-plugin'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,10 +12,13 @@ export default defineConfig({
     vue(),
     vueJsx(),
     vueDevTools(),
+    codeInspectorPlugin({
+      bundler: 'vite',
+    }),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 })
